d4e53056dfabb6420bb6ee2346464f8316eaf145
Karol Munoz <karol@karolmunoz.com>
2024-06-27 23:31:34 -0400

    update

---
+++ docs/config.js
@@ -0,0 +1,142 @@
+var config = {
+    style: 'mapbox://styles/kmunoz/clxxyra1c002f01p8eyw3d6wn',
+    accessToken: 'pk.eyJ1Ijoia211bm96IiwiYSI6ImNsY3A3NDloaDA2bnozcGxiN2U1Y2I2bWIifQ.WY4_mVStBm5c9CjvWsVy3w',
+    showMarkers: false,
+    markerColor: '#000000',
+    inset: false,
+    theme: 'dark',
+    use3dTerrain: false,
+    auto: false,
+    image: 'https://krlmnz.github.io/mtts/mini-logo.png',
+    title: 'Mini Takes The States',
+    subtitle: 'URBAN-X by MINI is going on an epic journey from Albuquerque, NM to Seattle, WA. Join us.',
+    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    chapters: [
+        {
+            id: 'balloon-fiesta-park',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Saturday, July 13',
+            description: '🎈 Balloon Fiesta Park · Albuquerque → Durango • 4 hour 48 min / 272.8 mi',
+            location: {
+                center: [-106.59896, 35.18873],
+                zoom: 10,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fort-lewis-college',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Sunday, July 14',
+            description: '🏰 Fort Lewis College • Durango, CO · Durango → Grand Junction • 3 hour 56 min / 164.5 mi',
+            location: {
+                center: [-107.88007, 37.27528],
+                zoom: 10,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'lincoln-park-complex',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Monday, July 15',
+            description: '🏞️ Lincoln Park Complex • Grand Junction, CO · Grand Junction → Salt Lake City • 5 hour 38 min / 349 mi',
+            location: {
+                center: [-108.55065, 39.07193],
+                zoom: 10,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'hardscrabble-ranch',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Thursday, July 18',
+            description: '🐮 Hardscrabble Ranch • Bozeman, MT · Bozeman → Missoula • 3 hour 52 min / 235 mi',
+            location: {
+                center: [-111.04293, 45.67699],
+                zoom: 10,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'washington-grizzly-stadium',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Friday, July 19',
+            description: '🐻 Washington Grizzly Stadium (U of MT) • Missoula, MT · Missoula → Spokane • 4 hour 40 min / 260 mi',
+            location: {
+                center: [-113.98218, 46.86011],
+                zoom: 10,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'comstock-park',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Saturday, July 20',
+            description: '🏞️ Comstock Park • Spokane, WA · Spokane → Yakima • 3 hour 8 min / 263 mi',
+            location: {
+                center: [-117.42605, 47.65878],
+                zoom: 10,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fullbright-park',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Monday, July 22',
+            description: '🏞️ SEATTLE, WA → RETURN HOME',
+            location: {
+                center: [-120.47232, 46.54566],
+                zoom: 10,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
